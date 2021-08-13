1e4e5f0ca3e15429130d2f3bc6f3039de888cf61
Inf0214 <frici0214@hotmail.com>
2021-08-13 10:52:23 +0200

    Switch back to HTML

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function onSubmit(token) {
 function onClick(e) {
   e.preventDefault();
   grecaptcha.ready(function() {
-    grecaptcha.execute('<?php echo SECRET_KEY?>', {action: 'submit'}).then(function(token) {
+    grecaptcha.execute('6Lfz-PkbAAAAAKUK4PEQa53OR5zGmc1iCyO-ChN9', {action: 'submit'}).then(function(token) {
         console.log(token);
         // Add your logic to submit to your backend server here.
     });
